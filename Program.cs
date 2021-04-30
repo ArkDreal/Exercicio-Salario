ea61e52971f1519e649799e779fcbe944cbc20bc
ArkDreal <andrey.o.leal@gmail.com>
2021-04-30 15:35:53 -0300

    Exercício do Salário concluído

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercício_Salário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console . WriteLine ( " Qual é o seu salário atual? " );
+            float  salarioAtual  =  float . Parse ( Console . ReadLine ());
+
+            if ( salarioAtual  <  500 ) {
+               float  salarioPorcentagem  = ( salarioAtual  /  100 ) *  30 ;
+               float  salarioCorreto  =  salarioAtual  +  salarioPorcentagem ;
+               Console . WriteLine ( " Seu salário corrigido é: "  +  salarioCorreto );
+            }
+
+            else {
+                Console . WriteLine ( " Você não tem direito ao benefício " );
+            }
+        }
+    }
+}
